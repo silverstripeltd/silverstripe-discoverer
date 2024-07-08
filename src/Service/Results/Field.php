7f286a57d8d8e01650101ca0e7ccddabe15d3daa
Chris Penny <chris.penny@gmail.com>
2024-07-09 07:45:46 +1200

    Add support for Field to return arrays of primitives (#7)

--- src/Service/Results/Field.php
+++ src/Service/Results/Field.php
@@ -2,7 +2,9 @@
 
 namespace SilverStripe\Discoverer\Service\Results;
 
+use ArrayIterator;
 use SilverStripe\View\ViewableData;
+use Traversable;
 
 class Field extends ViewableData
 {
@@ -14,7 +16,7 @@ class Field extends ViewableData
 
     public function forTemplate(): mixed
     {
-        // Attempt to return the formatted value (if available), or return raw as the fallback
+        // Attempt to use the formatted value (if available), or use the raw value as the fallback
         return $this->formatted ?? $this->raw;
     }
 
@@ -40,6 +42,43 @@ class Field extends ViewableData
         $this->formatted = $formatted;
 
         return $this;
+    }
+
+    public function getIterator(): Traversable
+    {
+        // Attempt to use the formatted value (if available), or use the raw value as the fallback
+        $value = $this->formatted ?? $this->raw;
+
+        if (!$value) {
+            return new ArrayIterator();
+        }
+
+        $arrayValue = is_array($value)
+            ? $this->convertArrayForTemplate($value)
+            : [$this];
+
+        return new ArrayIterator($arrayValue);
+    }
+
+    /**
+     * Silverstripe 5.3 will have native support for looping primitives in templates:
+     * https://github.com/silverstripe/silverstripe-framework/issues/11196
+     *
+     * We need to support versions of Silverstripe below 5.3 though, so we need this polyfill. It emulates the
+     * template implementation method from Silverstripe 5.3, so we should be able to remove this later without
+     * negatively impacting any project's template implementation
+     */
+    private function convertArrayForTemplate(array $array): array
+    {
+        $arrayList = [];
+
+        foreach ($array as $item) {
+            // Each item being looped needs to be a viewable data record. Reusing this class means that we also get
+            // support for any nested arrays
+            $arrayList[] = Field::create($item);
+        }
+
+        return $arrayList;
     }
 
 }
--- tests/Service/Results/FieldTest.php
+++ tests/Service/Results/FieldTest.php
@@ -2,6 +2,7 @@
 
 namespace SilverStripe\Discoverer\Tests\Service\Results;
 
+use ArrayIterator;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Discoverer\Service\Results\Field;
 
@@ -27,6 +28,41 @@ class FieldTest extends SapphireTest
 
         $this->assertEquals('raw', $fieldOne->forTemplate());
         $this->assertEquals('snippet', $fieldTwo->forTemplate());
+    }
+
+    public function testGetIterator(): void
+    {
+        $fieldOne = Field::create('raw');
+        $fieldTwo = Field::create([1, 2, 3, 4]);
+        $fieldThree = Field::create();
+        $fieldFour = Field::create([]);
+
+        /** @var ArrayIterator|Field[] $fieldOneIterator */
+        $fieldOneIterator = $fieldOne->getIterator();
+        /** @var ArrayIterator|Field[] $fieldTwoIterator */
+        $fieldTwoIterator = $fieldTwo->getIterator();
+        /** @var ArrayIterator|Field[] $fieldThreeIterator */
+        $fieldThreeIterator = $fieldThree->getIterator();
+        /** @var ArrayIterator|Field[] $fieldFourIterator */
+        $fieldFourIterator = $fieldFour->getIterator();
+
+        $this->assertCount(1, $fieldOneIterator);
+        $this->assertCount(4, $fieldTwoIterator);
+        $this->assertCount(0, $fieldThreeIterator);
+        $this->assertCount(0, $fieldFourIterator);
+
+        $this->assertEquals('raw', $fieldOneIterator->current()->getRaw());
+
+        $expected = [1, 2, 3, 4];
+        $results = [];
+
+        foreach ($fieldTwoIterator as $field) {
+            $this->assertInstanceOf(Field::class, $field);
+
+            $results[] = $field->getRaw();
+        }
+
+        $this->assertEqualsCanonicalizing($expected, $results);
     }
 
 }
