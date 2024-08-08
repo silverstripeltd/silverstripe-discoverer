99b5c69990a276c67e98613ce57fd3c087e844da
Chris Penny <chris.penny@gmail.com>
2024-08-08 13:00:34 +1200

    Add support for query suggestions (aka "did you mean")

---
+++ src/Query/Suggestion.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SilverStripe\Discoverer\Query;
+
+use SilverStripe\Core\Injector\Injectable;
+
+class Suggestion
+{
+
+    use Injectable;
+
+    public function __construct(private string $queryString, private ?int $limit = null, private array $fields = [])
+    {
+    }
+
+    public function getQueryString(): string
+    {
+        return $this->queryString;
+    }
+
+    public function setQueryString(string $queryString): void
+    {
+        $this->queryString = $queryString;
+    }
+
+    public function getLimit(): ?int
+    {
+        return $this->limit;
+    }
+
+    public function setLimit(?int $limit): self
+    {
+        $this->limit = $limit;
+
+        return $this;
+    }
+
+    public function getFields(): array
+    {
+        return $this->fields;
+    }
+
+    public function addField(string $fieldName): self
+    {
+        $this->fields[] = $fieldName;
+
+        return $this;
+    }
+
+}
