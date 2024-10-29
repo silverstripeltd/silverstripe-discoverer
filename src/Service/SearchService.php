7d0946fa3ba6e507c9cb209cc580481dcb4ba1fb
Chris Penny <chris.penny@gmail.com>
2024-10-30 07:31:43 +1300

    Add support for spelling suggestion feature

--- src/Service/SearchService.php
+++ src/Service/SearchService.php
@@ -6,6 +6,7 @@ use SilverStripe\Core\Injector\Injectable;
 use SilverStripe\Discoverer\Analytics\AnalyticsData;
 use SilverStripe\Discoverer\Query\Query;
 use SilverStripe\Discoverer\Query\Suggestion;
+use SilverStripe\Discoverer\Service\Interfaces\SpellingSuggestionAdaptor;
 use SilverStripe\Discoverer\Service\Results\Results;
 use SilverStripe\Discoverer\Service\Results\Suggestions;
 
@@ -16,13 +17,21 @@ class SearchService
 
     private ?SearchServiceAdaptor $adaptor = null;
 
+    private ?SpellingSuggestionAdaptor $spellingSuggestionAdaptor = null;
+
     private static array $dependencies = [
         'adaptor' => '%$' . SearchServiceAdaptor::class,
+        'spellingSuggestionAdaptor' => '%$' . SpellingSuggestionAdaptor::class,
     ];
 
     public function setAdaptor(?SearchServiceAdaptor $adaptor): void
     {
         $this->adaptor = $adaptor;
+    }
+
+    public function setSpellingSuggestionAdaptor(?SpellingSuggestionAdaptor $spellingSuggestionAdaptor): void
+    {
+        $this->spellingSuggestionAdaptor = $spellingSuggestionAdaptor;
     }
 
     public function search(Query $query, string $indexName): Results
@@ -33,6 +42,11 @@ class SearchService
     public function querySuggestion(Suggestion $suggestion, string $indexName): Suggestions
     {
         return $this->adaptor->querySuggestion($suggestion, $indexName);
+    }
+
+    public function spellingSuggestion(Suggestion $suggestion, string $indexName): Suggestions
+    {
+        return $this->spellingSuggestionAdaptor->process($suggestion, $indexName);
     }
 
     public function processAnalytics(AnalyticsData $analyticsData): void
--- src/Service/SearchServiceAdaptor.php
+++ src/Service/SearchServiceAdaptor.php
@@ -5,9 +5,19 @@ namespace SilverStripe\Discoverer\Service;
 use SilverStripe\Discoverer\Analytics\AnalyticsData;
 use SilverStripe\Discoverer\Query\Query;
 use SilverStripe\Discoverer\Query\Suggestion;
+use SilverStripe\Discoverer\Service;
 use SilverStripe\Discoverer\Service\Results\Results;
 use SilverStripe\Discoverer\Service\Results\Suggestions;
 
+/**
+ * Please be aware that we intend to replace this interface in v2. We want to move to a paradigm where each search
+ * function has its own interface and default (empty) implementation provided by Discoverer. This way, we are able to
+ * expand functionality in the future without causing breaking changes to this interface
+ *
+ * @see Service\Interfaces\SpellingSuggestionAdaptor
+ * @see Service\Adaptors\SpellingSuggestionAdaptor
+ * For an example of the new paradigm that we'll be using (one interface per search function)
+ */
 interface SearchServiceAdaptor
 {
 
